2443f7a785714ad6bf1a4ecc718eb4e1224c57f1
resious71 <38912146+resious71@users.noreply.github.com>
2024-01-18 16:38:05 +0900

    Create 05_01.py

---
+++ 텐서플로우 딥러닝/5과목_Deep Learning/05_01.py
@@ -0,0 +1,28 @@
+# 머신러닝과 딥러닝 ( 머신러닝, 딥러닝이란?, 두 분야의 관계, 머신러닝과 딥러닝의 비교 1 )
+#
+# 1장. 딥러닝 개념과 활용
+#  - 머신러닝과 딥러닝이란 무엇인가?
+#  1.1 머신러닝(Machine Learniung)
+#    - A computer program is said to learning from experience E with resprected some class of tasks T
+#      and performance measure P if its performance at tasks in T, as measured by P, improves with experience E
+#                                                                                 -- Tom Mitchell
+#  1.2 딥러닝(Deep Learning)
+#    - Deep Learning is a particular kind of machine learning that achieved great power and flexibility by 
+#      learning to represent the world as nested hierarchy of concepts, with each concept defined in releation 
+#      to simpler concepts, and more abstract representations computed in terms of less abstract ones
+#
+#  - 머신 러닝과 딥 러닝의 관계
+#    AI  >>   Machine Learning  >>  Deep Learning
+#
+#  - 머신 러닝과 딥 러닝의 비교
+#  2.1 데이터 의존도 
+#    - 딥러닝과 전통적인 머신러닝의 가장 큰 차이점은 데이터 양에 따른 성능
+#    - 딥러닝 알고리즘은 어떤 과제를 이해하기 위해서는 매우 큰 데이터가 필요하기 때문에 
+#      데이터가 작을 때는 성능이 잘 나오지 않으나 데이터의 양이 커지면 성능을 계속 향상시킬 수 있음
+#  
+#  2.2 하드웨어 의존도
+#    - 딥러닝 알고리즘은 고사양 머신이 많은 부분을 좌지우지 됨
+#    - 머신러닝은 저사양 머신에서도 실행이 가능
+#    - 딥러닝 알고리즘의 요구사항은 GPU가 포함되기 때문이고, GPU는 작업에서 숫자계산을 담당함
+#    - 딥러닝은 많은 양의 행렬 곱셍을 수행해야 하기 때문에 GPU를 사용하면 보다 효율적으로 최적화 할 수 있음
+#
